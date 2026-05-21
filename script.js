d5a5d19b2196c370d9c6eac8beab035540f148ca
manyalalhall <manyasinghlalhall@gmail.com>
2026-05-21 15:32:54 +0530

    initial_look

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateCalendar(){
+    const now= new Date();
+    const day=now.getDate();
+    const year=now.getFullYear();
+    const monthNames=["jaunuary","february","march","april","may","june","july","august","september","october","november","december"]
+    const month=monthNames[now.getMonth()];
+
+    document.getElementById('day').textContent=day;
+    document.getElementById('month').textContent=month;
+    document.getElementById('year').textContent=year;
+
+}
+
+updateCalendar();
